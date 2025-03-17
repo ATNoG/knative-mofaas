c34fdfcb64744571f727af9972b4111abb1712c7
Pedro Escaleira <pedroescaleira@hotmail.com>
2025-03-17 00:12:39 +0000

    other graphs almost done
    Signed-off-by: Pedro Escaleira <pedroescaleira@hotmail.com>

--- tests/resources/graphs.py
+++ tests/resources/graphs.py
@@ -10,14 +10,14 @@ SIZE_RATION = SIZE[1] / COMPARISON
 
 
 RESULTS_DIR = "results/"
+RESULT = "Memory"  # Memory or CPU
 
-# CONCURRENCY = "singular"            # singular or multiple
 
 def read_results_pod(file_path):
     results = []
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         content = f.read()
-                
+
     pos = 0
     while pos < len(content):
         # Locate the "HEADERS" marker
@@ -25,7 +25,7 @@ def read_results_pod(file_path):
         if headers_marker == -1:
             break
         # Locate the first '{' after "HEADERS"
-        header_start = content.find('{', headers_marker)
+        header_start = content.find("{", headers_marker)
         if header_start == -1:
             break
 
@@ -40,7 +40,7 @@ def read_results_pod(file_path):
         if body_marker == -1:
             break
         # Locate the first '{' after "BODY"
-        body_start = content.find('{', body_marker)
+        body_start = content.find("{", body_marker)
         if body_start == -1:
             break
 
@@ -50,29 +50,35 @@ def read_results_pod(file_path):
             body = {}
             next_index_body = body_start + 1
 
-        results.append({
-            "headers": headers,
-            "body": body,
-            "time": float(content[headers_marker - 18 - 28 - 16:headers_marker - 18 - 28])
-        })
-            
+        results.append(
+            {
+                "headers": headers,
+                "body": body,
+                "time": float(
+                    content[headers_marker - 18 - 28 - 16 : headers_marker - 18 - 28]
+                ),
+            }
+        )
+
         pos = next_index_body
     return results
 
 
 def read_requests_trace(file_path):
     results = {}
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         content = f.read()
-    for result in content.split("\n-------------------------------------------------------\n")[1:5000]:
+    for result in content.split(
+        "\n-------------------------------------------------------\n"
+    )[1:5000]:
         try:
             lines = result.split("\n")
-            response = json.loads(lines[2].split('Response: ')[1])
-            results[response['id']] = {
+            response = json.loads(lines[2].split("Response: ")[1])
+            results[response["id"]] = {
                 "response": response,
                 "start": float(lines[0].split("start: ")[1]),
                 "stop": float(lines[1].split("end: ")[1]),
-                "number": int(lines[1].split(' ')[1])
+                "number": int(lines[1].split(" ")[1]),
             }
         except Exception as e:
             print("Hmmm")
@@ -80,11 +86,14 @@ def read_requests_trace(file_path):
 
     return results
 
+
 def read_top(file_path):
     results = []
-    with open(file_path, 'r') as f:
+    with open(file_path, "r") as f:
         content = f.read()
-    for result in content.split("\n-------------------------------------------------------\n"):
+    for result in content.split(
+        "\n-------------------------------------------------------\n"
+    ):
         lines = result.split("\n")
         if not lines[0]:
             break
@@ -98,26 +107,32 @@ def read_top(file_path):
             if not l:
                 break
             info = l.split()
-            info[0] = info[0][:info[0].find("deployment") - 7]
+            info[0] = info[0][: info[0].find("deployment") - 7]
             if info[0] not in results[-1]["top"]:
                 results[-1]["top"][info[0]] = {}
-            results[-1]["top"][info[0]][info[1]] = {
-                "cpu": info[2],
-                "mem": info[3]
-            }
+            results[-1]["top"][info[0]][info[1]] = {"cpu": info[2], "mem": info[3]}
     return results
-            
+
+
+def sort_dict(d):
+    """Recursively sort the keys of a dictionary."""
+    if isinstance(d, dict):
+        return {
+            k: sort_dict(d[k]) if isinstance(d[k], dict) else d[k] for k in sorted(d)
+        }
+    return d
+
 
 def main():
     # This list will hold the results from all files
     all_results = {}
 
     # Iterate over items in the current directory
-    for item in os.listdir('results/'):
-        info = item.split('_')
-        auth_conc = info[2].split('-')[1]
-        verify_conc = info[3].split('-')[1]
-        concurrency = info[4].split('-')[1]
+    for item in os.listdir("results/"):
+        info = item.split("_")
+        auth_conc = info[2].split("-")[1]
+        verify_conc = info[3].split("-")[1]
+        concurrency = info[4].split("-")[1]
 
         if auth_conc not in all_results:
             all_results[auth_conc] = {}
@@ -133,12 +148,14 @@ def main():
             if filename == "top.txt":
                 all_results[auth_conc][verify_conc][concurrency] = read_top(file_path)
 
+    all_results = sort_dict(all_results)
+
+    unit = "MiB" if RESULT == "Memory" else "% of core usage"
     data = {
-        "Authorization Version": [],
-        "Verify Transaction Version": [],
-        "CPU": [],
-        "Memory": [],
-        
+        "Authorization version": [],
+        "Verify version": [],
+        f"CPU ({unit})": [],
+        f"Memory ({unit})": [],
     }
     for auth_conc in all_results:
         # if auth_conc == "attack":
@@ -152,18 +169,37 @@ def main():
                     memory = 0
                     for service in r["top"]:
                         for container in r["top"][service]:
-                            cpu += int(r["top"][service][container]['cpu'].split('m')[0])
-                            memory += int(r["top"][service][container]['mem'].split('Mi')[0])
-                    data["Authorization Version"].append(auth_conc if auth_conc != "attack" else f"{auth_conc} conc {concurrency}")
-                    data["Verify Transaction Version"].append(verify_conc if verify_conc != "attack" else f"{verify_conc} conc {concurrency}")
-                    data["CPU"].append(cpu)
-                    data["Memory"].append(memory)
-    
+                            cpu += int(
+                                r["top"][service][container]["cpu"].split("m")[0]
+                            )/10
+                            memory += int(
+                                r["top"][service][container]["mem"].split("Mi")[0]
+                            )
+                    data["Authorization version"].append(
+                        auth_conc
+                        if auth_conc != "attack"
+                        else f"c = {concurrency}"
+                    )
+                    data["Verify version"].append(
+                        verify_conc
+                        if verify_conc != "attack"
+                        else f"c = {concurrency}"
+                    )
+                    data[f"CPU ({unit})"].append(cpu)
+                    data[f"Memory ({unit})"].append(memory)
+
     # df = pd.DataFrame(data)
     # sns.set_theme(rc={"figure.figsize": SIZE})
     # ax = sns.scatterplot(data=data, x="Test", y="Relative frequency")
-    ax = sns.pointplot(data=data, x="Verify Transaction Version", hue="Authorization Version", y="Memory", linestyle="none", palette=sns.color_palette("colorblind"))
-    
+    ax = sns.pointplot(
+        data=data,
+        x="Authorization version",
+        hue="Verify version",
+        y=f"{RESULT} ({unit})",
+        linestyle="none",
+        palette=sns.color_palette("colorblind"),
+    )
+
     # for i, row in df.iterrows():
     #     # Get the x position corresponding to the categorical tick.
     #     # ax.get_xticks() returns the positions of the ticks, which should align with the order of your categories.
@@ -173,17 +209,25 @@ def main():
     #     y_pos = row["Relative frequency"]
     #     # Add an offset to y to avoid overlapping the marker
     #     ax.text(x=x_pos, y=y_pos + 0.02, s=f"{y_pos:.3f}", ha='center', va='bottom', fontdict={"size": 12*SIZE_RATION})
+
+    if RESULT == "CPU":
+        plt.ylim(0, 110)
+    else:
+        plt.ylim(0)
+    plt.title(f"{RESULT} used in each setup", fontdict={"size": 17 * SIZE_RATION})
     
-    # plt.ylim(0, 1)
-    # plt.title(f"Relative frequency of a successful attack with concurrency {'=' if CONCURRENCY == 'singular' else '>'} 1\n", fontdict={"size": 17*SIZE_RATION})
-    
-    ax.yaxis.label.set_fontsize(15*SIZE_RATION)
-    ax.xaxis.label.set_fontsize(15*SIZE_RATION)
-    ax.tick_params(labelsize=12*SIZE_RATION)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.grid(True)
+
+    ax.yaxis.label.set_fontsize(15 * SIZE_RATION)
+    ax.xaxis.label.set_fontsize(15 * SIZE_RATION)
+    ax.tick_params(labelsize=12 * SIZE_RATION)
     plt.tight_layout()
-    # plt.savefig(f"concurrency_{CONCURRENCY}.pdf")
+    plt.savefig(f"resources_{RESULT}.pdf")
 
     plt.show()
+
 
 if __name__ == "__main__":
     main()
